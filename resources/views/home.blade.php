e24e693752172128bd74d8fad3866de55ede7e65
pierobramanti <piero.bramanti94@gmail.com>
2024-09-23 16:56:41 +0200

    [UPD] chnaged name hello-world in "home" and printed hello world!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                
+                <h1>Hello World! </h1>
             </div>
         </div>
     </div>    
