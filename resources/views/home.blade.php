d298f55188b7c6a4c707e707a85c2cfa1deb2228
pierobramanti <piero.bramanti94@gmail.com>
2024-09-23 17:01:06 +0200

    [UPD] dynamic message "msg"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,9 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Hello World! </h1>
+                <h1>
+                {{$msg}}
+                </h1>
             </div>
         </div>
     </div>    
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $msg ="Hello World!";
+    return view('home', compact('msg'));
 });
